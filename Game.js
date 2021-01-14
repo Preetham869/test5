37df35094ff5ca9f1f1bc0cff7ce51e023e5b87d
Preetham869 <70512182+Preetham869@users.noreply.github.com>
2021-01-14 13:06:36 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,118 @@
+class Game {
+  constructor(){
+
+    Jet1_Img = loadImage("/Jet1.png");
+    Jet2_Img = loadImage("/Jet2.png");
+
+  }
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+  }
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+    }
+
+    Jet1 = createSprite(100,100);
+    Jet1.addImage("Jet1",Jet1_Img);
+    Jet1.scale = .2;
+
+   
+    Jet2 = createSprite(500,300);
+    Jet2.addImage("Jet2",Jet2_Img);
+    Jet2.scale = .3;
+
+    Jets = [Jet1, Jet2];
+  }
+
+  play(){
+    form.hide();
+
+    Player.getPlayerInfo();
+
+    if(allPlayers !== undefined){
+     background(rgb(198,135,103));
+     image(backgroundImg, 0,-displayHeight*4,displayWidth, displayHeight*5);
+      
+      //var display_position = 100;
+      
+      var index = 0;
+
+      var x = 10 ;
+      var y;
+
+      for(var plr in allPlayers){
+      
+        index = index + 1 ;
+        
+        x = x + 400;
+
+        y = displayHeight - 230 - allPlayers[plr].distance;
+        Jets[index-1].x = x;
+        Jets[index-1].y = y;
+
+          
+        if (index === player.index){
+         /* stroke(10);
+          fill("red");
+          ellipse(x,y,60,60);
+          Jets[index - 1].shapeColor = "red";*/
+        //  console.log(Jets[index - 1].y);
+        }
+      }
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance += 20
+      player.update();
+    }
+
+    if(keyIsDown(DOWN_ARROW) && player.index !== null){
+      player.distance -= 20
+      player.update();
+    }
+
+    if(player.distance > 1){
+      player.score = player.score + 1;
+      player.update();
+      // score.update();
+      resetJet(player.index);
+      resetJet(x);
+
+    }
+   
+  
+    drawSprites();
+
+  textSize(30);
+  fill("brown");
+  stroke("black");
+  strokeWeight(13);
+  text("Score : " + player.score,100,550)
+  text("Score : " + player.score,1000,550)
+
+  }
+ 
+  end(){
+    console.log("Game Ended");
+    console.log(player.rank);
+  }
+ 
+} 
---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,117 @@
+/*const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;*/
+
+// var engine, world;
+var canvas, backgroundImg;
+var Jet1,Jet2,Jets,Jet1_Img,Jet2_Img;
+var gameState = 0;
+var playerCount = 0;
+var allPlayers;
+var distance = 0;
+var score = 0;
+var database;
+var form, player, game;
+var asteroid,asteroid1,asteroidImage,asteroidGroup,asteroid1Group;
+
+function preload(){
+
+  backgroundImg = loadImage("/Background.jpg")
+  asteroidImage = loadImage("/asteroid.png")
+
+ }
+
+function setup() {
+  canvas = createCanvas(displayWidth - 250 , displayHeight - 150);
+
+  // engine = Engine.create();
+  // world = engine.world;
+  
+  database = firebase.database();
+
+  game = new Game();
+  game.getState();
+  game.start();
+
+  // World.add(world.border);
+
+  asteroidGroup = new Group();
+  asteroid1Group = new Group();
+
+  // Engine.run(engine);
+
+ } 
+
+function draw() {
+  
+  // Engine.update(engine);
+
+  background(backgroundImg)
+  line(displayWidth - 300, displayHeight-300, displayWidth - 300, displayHeight-300);
+
+  spawnAsteroid();
+  spawnAsteroid1();
+
+  if(playerCount === 2){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+
+  }
+  if(gameState === 2){
+    game.end();
+  }
+
+  if(Jet1.isTouching(asteroidGroup) || Jet1.isTouching(asteroid1Group)){
+
+    resetJet(); 
+
+  }
+
+ }
+
+ function spawnAsteroid(){
+
+  if (frameCount % 50 === 0) {
+    var asteroid = createSprite(-50,10,40,10);
+    asteroid.y = Math.round(random(100,480));
+    asteroid.addImage(asteroidImage);
+    asteroid.scale = 0.15;
+    asteroid.velocityX = 5;
+    asteroid.lifetime = 1000;
+    asteroidGroup.add(asteroid);
+  }
+   
+ }
+
+  function spawnAsteroid1(){
+
+  if (frameCount % 50 === 0) {
+    var asteroid1 = createSprite(1300,500,40,10);
+    asteroid1.y = Math.round(random(100,480));
+    asteroid1.addImage(asteroidImage);
+    asteroid1.scale = 0.15;
+    asteroid1.velocityX = -5;
+    asteroid1.lifetime = 1000;
+    asteroid1Group.add(asteroid1);
+  }
+  
+ }
+
+function resetJet(x){
+
+  if(x == 1){
+  Jet1.y = 634;
+  player.score = player.score + 1;
+}
+  else   {
+  Jet2.y = 634;
+  player.score = player.score + 1;
+
+}
+ }
+
+
+ 
